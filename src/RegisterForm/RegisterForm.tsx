32ea2f6a41b46dacf6d3e5a00e193b607f573c68
Hasmell <sssifart@mail.ru>
2025-04-25 16:05:24 +0300

    Исправил замечания в комментах Реакт 7 урок

--- src/RegisterForm/RegisterForm.tsx
+++ src/RegisterForm/RegisterForm.tsx
@@ -1,8 +1,10 @@
-import styles from './LoginForm.module.scss';
+import styles from './RegisterForm.module.scss';
 import { useState } from 'react';
+import { emailRegex } from '../constants.ts';
 
-export function LoginForm() {
+export function RegisterForm() {
 
+    const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState<Record<string, string>>({});
@@ -12,6 +14,11 @@ export function LoginForm() {
 
         const newErrors: Record<string, string> = {};
 
+        if (!email) {
+            newErrors.email = 'Email обязателен';
+        } else if (!emailRegex.test(email)) {
+            newErrors.email = 'Введите корректный email';
+        }
         if (username.length < 4) {
             newErrors.username = 'Username должен содержать минимум 4 символа';
         }
@@ -24,16 +31,34 @@ export function LoginForm() {
             return;
         }
 
-        console.log(`Пользователь успешно авторизован\nЛогин: ${username}\nПароль: ${password}`);
-    }
+        console.log(`Пользователь успешно зарегистрирован\nЛогин: ${username}\nПароль: ${password}`);
+    };
 
     return (
-        <form className={styles.loginForm} onSubmit={handleSubmit}>
-            <p className={styles.loginFormTitle}>Login Form</p>
+        <form className={styles.registerForm} onSubmit={handleSubmit}>
+            <p className={styles.registerFormTitle}>Register Form</p>
+
+            <input
+                type="email"
+                name="email"
+                className={styles.registerEmailInput}
+                placeholder="Email"
+                value={email}
+                onChange={e => {
+                    setEmail(e.target.value);
+                    setErrors(prev => ({ ...prev, email: '' }));
+                }}
+            />
+            {errors.email && (
+                <span className={styles.errorText}>
+        {errors.email}
+      </span>
+            )}
+
             <input
                 type="text"
                 name="username"
-                className={styles.usernameInput}
+                className={styles.registerUsernameInput}
                 placeholder="Username"
                 value={username}
                 onChange={e => {
@@ -46,24 +71,26 @@ export function LoginForm() {
                 {errors.username}
       </span>
             )}
+
             <input
                 type="password"
                 name="password"
-                className={styles.passwordInput}
+                className={styles.registerPasswordInput}
                 placeholder="Password"
                 value={password}
-                onChange={(e) => {
+                onChange={e => {
                     setPassword(e.target.value);
                     setErrors(prev => ({ ...prev, password: '' }));
-                    }}
+                }}
             />
             {errors.password && (
                 <span className={styles.errorText}>
         {errors.password}
       </span>
             )}
-            <button type="submit" className={styles.loginButton}>
-                LOGIN
+
+            <button type="submit" className={styles.registerButton}>
+                WELCOME TO THE CLUB, BUDDY
             </button>
         </form>
     );
